058c2a4d7761c40b32df218d425caa6a4081a862
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-04-23 19:38:54 -0600

    add student form

--- src/app/student-form/student-form.component.ts
+++ src/app/student-form/student-form.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-student-form',
   templateUrl: './student-form.component.html',
   styleUrls: ['./student-form.component.scss'],
+  standalone: false,
 })
-export class StudentFormComponent  implements OnInit {
-
-  constructor() { }
+export class StudentFormComponent implements OnInit {
+  constructor(private navCtrl: NavController) {}
 
   ngOnInit() {}
 
+  closeForm() {
+    this.navCtrl.navigateBack('/tabs/tab1');
+  }
+
+  onSubmit() {
+    console.log('Form submitted!');
+  }
 }
