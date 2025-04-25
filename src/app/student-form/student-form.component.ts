cf8b9d2dfbb288d82b2c536a4abd6ca99db5ec06
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-04-24 19:28:15 -0600

    reactive form validation

--- src/app/student-form/student-form.component.ts
+++ src/app/student-form/student-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { SchoolService } from '../school.service';
 import { Student } from '../student';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student-form',
@@ -12,27 +13,35 @@ import { Student } from '../student';
 export class StudentFormComponent implements OnInit {
   constructor(
     private navCtrl: NavController,
-    private schoolService: SchoolService
+    private schoolService: SchoolService,
+    private formBuilder: FormBuilder
   ) {}
 
-  student: Student = {
-    id: 0,
-    name: '',
-    age: 0,
-    level: '',
-  };
+  student!: FormGroup;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.student = this.formBuilder.group({
+      id: [0, [Validators.required]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      age: [18, [Validators.required, Validators.min(18)]],
+      level: ['', [Validators.required]],
+    });
+  }
+
+  get name() {
+    return this.student.get('name');
+  }
 
   closeForm() {
     this.navCtrl.navigateBack('/tabs/tab1');
   }
 
   onSubmit() {
-    console.log('Form submitted!', this.student);
-    this.schoolService.addStudent(this.student).subscribe(
+    console.log('Form submitted!', this.student.value);
+    this.schoolService.addStudent(this.student.value).subscribe(
       (response) => {
         console.log('Student added!', response);
+        this.student.reset(); // Reset the form after submission
         this.navCtrl.navigateBack('/tabs/tab1');
       },
       (error) => {
