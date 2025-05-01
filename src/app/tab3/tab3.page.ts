94c853af5f430a3bb98e046ad4b9d4eb0a5bedaa
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-04-30 18:56:29 -0600

    email composer & ignore android

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailComposer } from 'capacitor-email-composer';
 
 @Component({
   selector: 'app-tab3',
@@ -7,7 +8,23 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab3Page {
-
   constructor() {}
 
+  openEmail() {
+    EmailComposer.hasAccount()
+      .then((result) => {
+        if (result) {
+          console.log('hasAccount:', result);
+          // open the email composer
+          EmailComposer.open({
+            to: ['sharegh.yusefi@robogarden.ca'],
+            subject: 'Email Button Clicked',
+            body: 'This is a test email from the Ionic app.',
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error checking email account:', error);
+      });
+  }
 }
